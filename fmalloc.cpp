62748020457e15e2d91f51eeb11740a96df47f3d
Edoardo Canepa <aerisarn@gmail.com>
2024-03-31 18:15:23 +0200

    Oldest page swap policy, single swap file

--- fmalloc.cpp
+++ fmalloc.cpp
@@ -2,16 +2,16 @@
 #include "o1heap.h"
 
 #include <io.h>
-#include <windows.h>
 #include <sys/types.h>
+#include <windows.h>
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 
-#include <vector>
 #include <string>
+#include <vector>
 
 #ifdef _XBOX_UWP
 extern "C" {
@@ -26,172 +26,172 @@ RemoveVectoredExceptionHandler(_In_ PVOID Handle);
 }
 #endif
 
-#define MAPFILE_SIZE 536870912ull
-
-struct MapFileDescriptor
-{
-	int file_descriptor;
-	HANDLE mapping_handle;
-};
-
-MapFileDescriptor INVALID_MAPFILE = { -1, NULL };
-
-LPVOID fmalloced_base = nullptr;
-std::vector<MapFileDescriptor> descriptors;
-HANDLE view_handle1;
-HANDLE view_handle2;
-size_t next_replace = 0;
-HANDLE exception_handler = NULL;
-
-CRITICAL_SECTION memory_critical_section; 
-
-
-static O1HeapInstance* instance = NULL;
-
-#ifdef __USE_FILE_OFFSET64
-# define DWORD_HI(x) (x >> 32)
-# define DWORD_LO(x) ((x) & 0xffffffff)
-#else
-# define DWORD_HI(x) (0)
-# define DWORD_LO(x) (x)
+#if ULONG_MAX / 2 < LONG_MAX
+#error `unsigned long` too narrow.  Need new approach.
 #endif
 
-size_t inline get_file_to_map(void* address)
-{
-	return ((unsigned long long)address - (unsigned long long)fmalloced_base) / MAPFILE_SIZE;
+unsigned long distance(long x, long y) {
+  return (x > y) ? (unsigned long)x - (unsigned long)y
+                 : (unsigned long)y - (unsigned long)x;
 }
 
+#define MAPFILE_MAX_LOADED_BYTES 1073741824ull
+#define MAPFILE_PAGE_SIZE 65536
 
-int fallocate(HANDLE hndl, long long int size_to_reserve)
-{
-    if (size_to_reserve <= 0)
-        return 0;
+struct MapFileDescriptor {
+  int file_descriptor;
+  HANDLE mapping_handle;
+};
 
-    LARGE_INTEGER minus_one = {0}, zero = {0};
-    minus_one.QuadPart = -1;
+MapFileDescriptor INVALID_MAPFILE = {-1, NULL};
 
-    // Get the current file position
-    LARGE_INTEGER old_pos = {0};
-    if (!SetFilePointerEx(hndl, zero, &old_pos, FILE_CURRENT))
-        return -1;
+LPVOID fmalloced_base = nullptr;
+MapFileDescriptor swap_file;
+std::vector<HANDLE> views;
+std::vector<long> views_last_swap;
+HANDLE exception_handler = NULL;
+size_t pages = 0;
 
-    // Movie file position to the new end. These calls do NOT result in the actual allocation of
-    // new blocks, but they must succeed.
-    LARGE_INTEGER new_pos = {0};
-    new_pos.QuadPart      = size_to_reserve;
-    if (!SetFilePointerEx(hndl, new_pos, NULL, FILE_END))
-        return -1;
-    if (!SetEndOfFile(hndl))
-        return -1;
+long time = 0;
 
-    if (!SetFilePointerEx(hndl, minus_one, NULL, FILE_END))
-        return -1;
-    char  initializer_buf[1] = {1};
-    DWORD written            = 0;
-    if (!WriteFile(hndl, initializer_buf, 1, &written, NULL))
-        return -1;
+CRITICAL_SECTION memory_critical_section;
 
+static O1HeapInstance *instance = NULL;
+
+#define DWORD_HI(x) (x >> 32)
+#define DWORD_LO(x) ((x)&0xffffffff)
+
+size_t inline get_view_to_map(void *address) {
+  return ((unsigned long long)address - (unsigned long long)fmalloced_base) /
+         MAPFILE_PAGE_SIZE;
+}
+
+size_t inline get_view_to_unmap() {
+  size_t index = 0;
+  long max_distance = 0;
+  for (size_t v = 0; v < views_last_swap.size(); ++v) {
+    long component_distance = distance(time, views_last_swap.at(v));
+    if (component_distance > max_distance) {
+      index = v;
+      max_distance = component_distance;
+    }
+  }
+  return index;
+}
+
+int fallocate(HANDLE hndl, long long int size_to_reserve) {
+  if (size_to_reserve <= 0)
     return 0;
+
+  LARGE_INTEGER minus_one = {0}, zero = {0};
+  minus_one.QuadPart = -1;
+
+  // Get the current file position
+  LARGE_INTEGER old_pos = {0};
+  if (!SetFilePointerEx(hndl, zero, &old_pos, FILE_CURRENT))
+    return -1;
+
+  // Movie file position to the new end. These calls do NOT result in the actual
+  // allocation of new blocks, but they must succeed.
+  LARGE_INTEGER new_pos = {0};
+  new_pos.QuadPart = size_to_reserve;
+  if (!SetFilePointerEx(hndl, new_pos, NULL, FILE_END))
+    return -1;
+  if (!SetEndOfFile(hndl))
+    return -1;
+
+  if (!SetFilePointerEx(hndl, minus_one, NULL, FILE_END))
+    return -1;
+  char initializer_buf[1] = {1};
+  DWORD written = 0;
+  if (!WriteFile(hndl, initializer_buf, 1, &written, NULL))
+    return -1;
+
+  return 0;
 }
 
-void nemory_mapping_init(const char* files_prefix, size_t size);
+void nemory_mapping_init(const char *files_prefix, size_t size);
 void nemory_mapping_deinit();
 
 /* init routine */
-void fmalloc_init(const char* filepath, size_t max_size)
-{
-	nemory_mapping_init(filepath, max_size);
-	instance = o1heapInit(fmalloced_base, max_size);
-	if (instance == NULL)
-	{
-		return;
-	}
-       
-        if (!InitializeCriticalSectionAndSpinCount(&memory_critical_section,
-                                                   0x00000400)) {
-                return;
-        }
+void fmalloc_init(const char *filepath, size_t max_size) {
+  nemory_mapping_init(filepath, max_size);
+  instance = o1heapInit(fmalloced_base, max_size);
+  if (instance == NULL) {
+    return;
+  }
+
+  if (!InitializeCriticalSectionAndSpinCount(&memory_critical_section,
+                                             0x00000400)) {
+    return;
+  }
 }
 
-void *fmalloc(size_t size)
-{
-        EnterCriticalSection(&memory_critical_section);    // no time-out interval
-	void* result = o1heapAllocate(instance, size);
-        LeaveCriticalSection(&memory_critical_section);
-        return result;
+void *fmalloc(size_t size) {
+  EnterCriticalSection(&memory_critical_section); // no time-out interval
+  void *result = o1heapAllocate(instance, size);
+  LeaveCriticalSection(&memory_critical_section);
+  return result;
 }
 
-void ffree(void *addr)
-{
-        EnterCriticalSection(&memory_critical_section); // no time-out interval
-   o1heapFree(instance, addr);
-        LeaveCriticalSection(&memory_critical_section);
+void ffree(void *addr) {
+  EnterCriticalSection(&memory_critical_section); // no time-out interval
+  o1heapFree(instance, addr);
+  LeaveCriticalSection(&memory_critical_section);
 }
 
-void* frealloc(void *_Block, size_t _Size) {
-   EnterCriticalSection(&memory_critical_section); // no time-out interval
-   void *new_alloc = fmalloc(_Size);
-   memcpy(new_alloc, _Block, _Size);
-   ffree(_Block);
-   LeaveCriticalSection(&memory_critical_section);
-   return new_alloc;
+void *frealloc(void *_Block, size_t _Size) {
+  EnterCriticalSection(&memory_critical_section); // no time-out interval
+  void *new_alloc = fmalloc(_Size);
+  memcpy(new_alloc, _Block, _Size);
+  ffree(_Block);
+  LeaveCriticalSection(&memory_critical_section);
+  return new_alloc;
 }
 
-XBOXFMALLOC_API void  fmalloc_close()
-{
-	instance = nullptr;
-	nemory_mapping_deinit();
-        DeleteCriticalSection(&memory_critical_section);
+XBOXFMALLOC_API void fmalloc_close() {
+  instance = nullptr;
+  nemory_mapping_deinit();
+  DeleteCriticalSection(&memory_critical_section);
 }
 
-MapFileDescriptor open_map_file(const char* filepath, size_t max_size)
-{
-	struct _stat64 st;
+MapFileDescriptor open_map_file(const char *filepath, size_t max_size) {
+  struct _stat64 st;
 
-	if (_stat64(filepath, &st) < 0 || st.st_size < max_size)
-	{
-		//try to create it
-		int fd = _open(filepath, O_RDWR | O_CREAT, 0644);
-		if (fd < 0)
-		{
-			return INVALID_MAPFILE;
-		}
+  if (_stat64(filepath, &st) < 0 || st.st_size < max_size) {
+    // try to create it
+    int fd = _open(filepath, O_RDWR | O_CREAT, 0644);
+    if (fd < 0) {
+      return INVALID_MAPFILE;
+    }
 
-		if (fallocate((HANDLE)_get_osfhandle(fd), max_size) != 0)
-		{
-			return INVALID_MAPFILE;
-		}
-		_close(fd);
+    if (fallocate((HANDLE)_get_osfhandle(fd), max_size) != 0) {
+      return INVALID_MAPFILE;
+    }
+    _close(fd);
 
+    if (_stat64(filepath, &st) < 0) {
+      return INVALID_MAPFILE;
+    }
+  }
 
-		if (_stat64(filepath, &st) < 0)
-		{
-			return INVALID_MAPFILE;
-		}
-	}
+  int fd = _open(filepath, O_RDWR, 0644);
+  if (fd < 0) {
+    return INVALID_MAPFILE;
+  }
 
-	int fd = _open(filepath, O_RDWR, 0644);
-	if (fd < 0) {
-		return INVALID_MAPFILE;
-	}
-
-	HANDLE section = CreateFileMapping(
-		(HANDLE)_get_osfhandle(fd),
-		nullptr,
-		PAGE_READWRITE,
-		DWORD_HI(max_size), DWORD_LO(max_size), nullptr
-	);
-	if (NULL == section)
-	{
-		return INVALID_MAPFILE;
-	}
-	return  { fd, section };
+  HANDLE section =
+      CreateFileMapping((HANDLE)_get_osfhandle(fd), nullptr, PAGE_READWRITE,
+                        DWORD_HI(max_size), DWORD_LO(max_size), nullptr);
+  if (NULL == section) {
+    return INVALID_MAPFILE;
+  }
+  return {fd, section};
 }
 
-void close_map_file(MapFileDescriptor map_file)
-{
-	CloseHandle(map_file.mapping_handle);
-	_close(map_file.file_descriptor);
+void close_map_file(MapFileDescriptor map_file) {
+  CloseHandle(map_file.mapping_handle);
+  _close(map_file.file_descriptor);
 }
 
 #ifdef _XBOX_UWP
@@ -206,109 +206,94 @@ void close_map_file(MapFileDescriptor map_file)
 #define VAlloc VirtualAlloc2
 #endif
 
-//THE SHADOW!
+// THE SHADOW!
+// The handler is handled by process, so it's not thread safe
 static LONG CALLBACK
 ShadowExceptionHandler(PEXCEPTION_POINTERS exception_pointers) {
-	void* addr =
-		(void*)(exception_pointers->ExceptionRecord->ExceptionInformation[1]);
+  void *addr =
+      (void *)(exception_pointers->ExceptionRecord->ExceptionInformation[1]);
 
-	size_t file_index = get_file_to_map(addr);
+  size_t view_index = get_view_to_map(addr);
 
-	if (file_index >= descriptors.size())
-		return EXCEPTION_CONTINUE_SEARCH;
+  if (view_index >= pages)
+    return EXCEPTION_CONTINUE_SEARCH;
 
-        HANDLE &unload_handle = next_replace ? view_handle2 : view_handle1;
+  //We got work to do. Lock
+  EnterCriticalSection(&memory_critical_section);
 
-        //VirtualLock(unload_handle, MAPFILE_SIZE);
+  time += 1;
 
-	UnmapViewOfFile2(GetCurrentProcess(), unload_handle,
-                         MEM_PRESERVE_PLACEHOLDER);
+  size_t view_to_swap = get_view_to_unmap();
 
-	unload_handle = MapFile(
-		descriptors.at(file_index).mapping_handle,
-		nullptr,
-		(char*)fmalloced_base + file_index * MAPFILE_SIZE,
-		0,
-		MAPFILE_SIZE,
-		MEM_REPLACE_PLACEHOLDER,
-		PAGE_READWRITE,
-		nullptr, 0);
+  HANDLE &swap_handle = views[view_to_swap];
 
-        //VirtualUnlock(unload_handle, MAPFILE_SIZE);
+  UnmapViewOfFile2(GetCurrentProcess(), swap_handle,
+                   MEM_PRESERVE_PLACEHOLDER);
 
-        next_replace = next_replace ? 0 : 1;
+  swap_handle = MapFile(swap_file.mapping_handle, nullptr,
+                        (char *)fmalloced_base + view_index * MAPFILE_PAGE_SIZE,
+                        view_index * MAPFILE_PAGE_SIZE, MAPFILE_PAGE_SIZE,
+                        MEM_REPLACE_PLACEHOLDER, PAGE_READWRITE,
+                          nullptr, 0);
 
-	return EXCEPTION_CONTINUE_EXECUTION;
+  views_last_swap[view_to_swap] = time;
+
+  LeaveCriticalSection(&memory_critical_section);
+
+  return EXCEPTION_CONTINUE_EXECUTION;
 }
 
-void nemory_mapping_init(const char* files_prefix, size_t size)
-{
-	size_t pages = (size / MAPFILE_SIZE);
-	pages += (size % MAPFILE_SIZE > 0) ? 1 : 0;
-	descriptors.resize(pages);
-	for (size_t index = 0; index < pages; ++index)
-	{
-		std::string file_name = files_prefix + std::to_string(index);
-		descriptors[index] = open_map_file(file_name.c_str(), MAPFILE_SIZE);
-	}
+void nemory_mapping_init(const char *files_prefix, size_t size) {
+  swap_file = open_map_file(files_prefix, size);
 
-	//allocate the whole address space
-	fmalloced_base = VAlloc(
-		nullptr,
-		nullptr,
-		pages * MAPFILE_SIZE,
-		MEM_RESERVE | MEM_RESERVE_PLACEHOLDER,
-		PAGE_NOACCESS,
-		nullptr, 0
-	);
+  pages = (size / MAPFILE_PAGE_SIZE);
 
-	//split it in file pages
-	for (size_t index = 0; index < pages - 1; ++index)
-	{
-		if (!VirtualFree(
-			(char*)fmalloced_base + index * MAPFILE_SIZE,
-			MAPFILE_SIZE,
-			MEM_RELEASE | MEM_PRESERVE_PLACEHOLDER
-		))
-		{
-			int error = GetLastError();
-			fmalloced_base = nullptr;
-		};
-	}
+  // allocate the whole address space
+  fmalloced_base =
+      VAlloc(nullptr, nullptr, size,
+             MEM_RESERVE | MEM_RESERVE_PLACEHOLDER, PAGE_NOACCESS, nullptr, 0);
 
-	//load first page
-        // load first two page
-        view_handle1 = MapFile(
-            descriptors.at(0).mapping_handle, nullptr, fmalloced_base, 0,
-            MAPFILE_SIZE, MEM_REPLACE_PLACEHOLDER, PAGE_READWRITE, nullptr, 0);
+  // split it in file pages
+  for (size_t index = 0; index < pages - 1; ++index) {
+    if (!VirtualFree((char *)fmalloced_base + index * MAPFILE_PAGE_SIZE,
+                     MAPFILE_PAGE_SIZE,
+                     MEM_RELEASE | MEM_PRESERVE_PLACEHOLDER)) {
+      int error = GetLastError();
+      fmalloced_base = nullptr;
+    };
+  }
 
-        view_handle2 =
-            MapFile(descriptors.at(1).mapping_handle, nullptr,
-                                      (char *)fmalloced_base + MAPFILE_SIZE, 0,
-                                      MAPFILE_SIZE, MEM_REPLACE_PLACEHOLDER,
-                                      PAGE_READWRITE, nullptr, 0);
+  size_t pages_to_load = MAPFILE_MAX_LOADED_BYTES / MAPFILE_PAGE_SIZE;
+  views.resize(pages_to_load);
+  views_last_swap.resize(pages_to_load, 0);
 
-	exception_handler = AddVectoredExceptionHandler(TRUE, &ShadowExceptionHandler);
+  for (size_t index = 0; index < pages_to_load; ++index) {
+    views[index] = MapFile(swap_file.mapping_handle, nullptr,
+                           (char*)fmalloced_base + index * MAPFILE_PAGE_SIZE,
+                           index * MAPFILE_PAGE_SIZE, MAPFILE_PAGE_SIZE,
+                           MEM_REPLACE_PLACEHOLDER, PAGE_READWRITE, nullptr, 0);
+    if (views[index] == NULL)
+    {
+      int error = GetLastError();
+      fmalloced_base = nullptr;
+    }
+  }
+
+  exception_handler =
+      AddVectoredExceptionHandler(TRUE, &ShadowExceptionHandler);
 }
 
-void nemory_mapping_deinit()
-{
-        RemoveVectoredExceptionHandler(exception_handler);
+void nemory_mapping_deinit() {
+  RemoveVectoredExceptionHandler(exception_handler);
 
-	exception_handler = NULL;
+  exception_handler = NULL;
 
-	UnmapViewOfFile2(GetCurrentProcess(), view_handle1, MEM_PRESERVE_PLACEHOLDER);
-	view_handle1 = NULL;
+  for (size_t index = 0; index < views.size(); ++index) {
+    UnmapViewOfFile(views.at(index));
+  }
 
-        UnmapViewOfFile2(GetCurrentProcess(), view_handle2, MEM_PRESERVE_PLACEHOLDER);
-        view_handle2 = NULL;
+  VirtualFree(fmalloced_base, 0, MEM_RELEASE);
+  fmalloced_base = nullptr;
 
-	size_t pages = descriptors.size();
-	VirtualFree(fmalloced_base, 0, MEM_RELEASE);
-	fmalloced_base = nullptr;
-
-	for (auto& descriptor : descriptors)
-	{
-		close_map_file(descriptor);
-	}
+  close_map_file(swap_file);
 }
